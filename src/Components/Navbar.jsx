084fb261a0965820cd7460f3b352c9900983be02
behruzcoder <behruz@gmail.co>
2025-11-18 16:36:18 +0500

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import Logo from '../assets/img1.svg'
 
 export default function Navbar() {
   return (
     <div>
-      <h1 className=''>navbar</h1>
+      <nav className='container mx-auto flex justify-between items-center mt-4'>
+
+
+        <a href="#!">
+            <img src={Logo} alt="" />
+        </a>
+
+        <ul className='flex gap-5 items-center'>
+            <li>
+                <a href="#!">Home</a>
+            </li>
+            <li>
+                <a href="#!">Features</a>
+            </li>
+            <li>
+                <a href="#!">Pricing</a>
+            </li>
+            <li>
+                <a href="#!">Blog</a>
+            </li>
+        </ul>
+
+        <button>Get Started</button>
+      </nav>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+   container:{
+    padding: '15px',
+    center:true,
+   },
     extend: {},
+    
   },
   plugins: [],
 }
